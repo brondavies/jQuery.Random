e9e03daa0e9c0b522e25a79f6aa3cf43f14674e4
brondavies <git@brontech.com>
2013-07-11 10:44:14 -0500

    Added a function to select a random element
    jQuery.random() extension allows you to return a random element of the
    set from the results of a selector

--- jquery.random.js
+++ jquery.random.js
@@ -5,3 +5,9 @@
     n = parseInt(n);
     return Math.floor( Math.random() * (n - m + 1) ) + m;
 }
+// jQuery random() 1.01
+$.fn.random = function () {
+    if (this.length == 0) return this;
+    var i = jQuery.Random(0, this.length - 1);
+    return this.slice(i, i+1);
+}
